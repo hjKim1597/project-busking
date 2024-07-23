aa2bd50c27b636339c31444fccee70ba2c0161ce
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-07-23 17:39:27 +0900

    Update main 컨트롤러 수정

--- src/main/java/com/busking/main/controller/MainController.java
+++ src/main/java/com/busking/main/controller/MainController.java
@@ -33,10 +33,12 @@ public class MainController extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doAction(request, response);
 	}
 	
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doAction(request, response);
 	}
 	
 	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
