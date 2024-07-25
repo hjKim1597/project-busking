f9403678d8a83bceb4ff4ef0e36170a7942a690c
hjkim1597 <hojinkim971597@gmail.com>
2024-07-25 20:39:35 +0900

    update 관리자 0725

---
+++ src/main/java/com/busking/admin/controller/AdminController.java
@@ -0,0 +1,60 @@
+package com.busking.admin.controller;
+
+import java.io.IOException;
+
+import com.busking.admin.service.AdminService;
+import com.busking.admin.service.AdminServiceImpl;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("*.admin")
+public class AdminController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public AdminController() {
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doAction(request, response);
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doAction(request, response);
+
+	}
+
+	protected void doAction(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("utf-8");
+
+		String uri = request.getRequestURI();
+		String path = request.getContextPath();
+		String command = uri.substring(path.length());
+
+		System.out.println(command);
+
+		AdminService service;
+
+		if (command.equals("/adminPage.admin")) {
+			String page = request.getParameter("page");
+			if(page == null) page = "1";
+			request.setAttribute("page", page);
+			
+			service = new AdminServiceImpl();
+			service.getList(request, response);
+		}
+
+	}
+
+}
--- src/main/java/com/busking/admin/model/AdminPageDTO.java
+++ src/main/java/com/busking/admin/model/AdminPageDTO.java
@@ -1,83 +1,137 @@
 package com.busking.admin.model;
 
+import java.sql.Date;
+
 public class AdminPageDTO {
 
-	private String managerId;
+	private String userId;
+	private String userName;
+	private Date resDate;
+	private String resLoca;
+	private String resAmp;
+	private String resContent;
+	private int resCount;
+	private int userPno;
+	private String userEmail;
+	private String userAddr;
+	private String manangerId;
 	private String managerPw;
-	private String managerEmail;
-	private int locaId;
-	
-	
+
 	public AdminPageDTO() {
 	}
 
-
-	public AdminPageDTO(String managerId, String managerPw, String managerEmail, int locaId) {
+	public AdminPageDTO(String userId, String userName, Date resDate, String resLoca, String resAmp, String resContent,
+			int resCount, int userPno, String userEmail, String userAddr, String manangerId, String managerPw) {
 		super();
-		this.managerId = managerId;
+		this.userId = userId;
+		this.userName = userName;
+		this.resDate = resDate;
+		this.resLoca = resLoca;
+		this.resAmp = resAmp;
+		this.resContent = resContent;
+		this.resCount = resCount;
+		this.userPno = userPno;
+		this.userEmail = userEmail;
+		this.userAddr = userAddr;
+		this.manangerId = manangerId;
 		this.managerPw = managerPw;
-		this.managerEmail = managerEmail;
-		this.locaId = locaId;
 	}
 
-
-	public String getManagerId() {
-		return managerId;
+	public String getUserId() {
+		return userId;
 	}
 
-
-	public void setManagerId(String managerId) {
-		this.managerId = managerId;
+	public void setUserId(String userId) {
+		this.userId = userId;
 	}
 
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Date getResDate() {
+		return resDate;
+	}
+
+	public void setResDate(Date resDate) {
+		this.resDate = resDate;
+	}
+
+	public String getResLoca() {
+		return resLoca;
+	}
+
+	public void setResLoca(String resLoca) {
+		this.resLoca = resLoca;
+	}
+
+	public String getResAmp() {
+		return resAmp;
+	}
+
+	public void setResAmp(String resAmp) {
+		this.resAmp = resAmp;
+	}
+
+	public String getResContent() {
+		return resContent;
+	}
+
+	public void setResContent(String resContent) {
+		this.resContent = resContent;
+	}
+
+	public int getResCount() {
+		return resCount;
+	}
+
+	public void setResCount(int resCount) {
+		this.resCount = resCount;
+	}
+
+	public int getUserPno() {
+		return userPno;
+	}
+
+	public void setUserPno(int userPno) {
+		this.userPno = userPno;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserAddr() {
+		return userAddr;
+	}
+
+	public void setUserAddr(String userAddr) {
+		this.userAddr = userAddr;
+	}
+
+	public String getManangerId() {
+		return manangerId;
+	}
+
+	public void setManangerId(String manangerId) {
+		this.manangerId = manangerId;
+	}
 
 	public String getManagerPw() {
 		return managerPw;
 	}
 
-
 	public void setManagerPw(String managerPw) {
 		this.managerPw = managerPw;
 	}
-
-
-	public String getManagerEmail() {
-		return managerEmail;
-	}
-
-
-	public void setManagerEmail(String managerEmail) {
-		this.managerEmail = managerEmail;
-	}
-
-
-	public int getLocaId() {
-		return locaId;
-	}
-
-
-	public void setLocaId(int locaId) {
-		this.locaId = locaId;
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	
 	
